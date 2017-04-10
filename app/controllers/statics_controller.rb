297c9bed6d1115f291182a18af7d16b1b4a1747d
timfesta <timothyfesta@yahoo.com>
2017-04-10 05:54:00 -0700

    statics controller & production.rb

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,2 +1,17 @@
 class StaticsController < ApplicationController
+
+	def home
+		# @products = Product.all
+	end
+
+	def top
+	end
+
+	def templates
+		# @products = Product.all
+	end
+
+	def contact
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
   
-	root 'statics#home'
+	get 'signup' => 'users#new'
+	get 'profile' => 'users#show'
+	resources :users
+
+	get 'login'   => 'sessions#new'
+	get 'logout'  => 'sessions#destroy'
+	resources :sessions
+
+	get 'topseller' => 'photos#top_templates'
+	resources :photos
+
+  	resources :charges
+  	resources :purchases, only: [:show]
+  	resources :products 
+
+  	get 'templates' => 'statics#templates'
+  	get 'contact' => 'statics#contact'
+  	get 'about' => 'statics#about'
+  	get 'socialmedia' => 'statics#socialmedia'
+    get 'tutorials' => 'statics#tutorials'
+  	resources :statics
+
+  	root 'statics#home'
 
 end
