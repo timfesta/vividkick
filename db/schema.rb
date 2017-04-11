8ce902713429cb60d37d00f39deb4137e5fcab5b
timfesta <timothyfesta@yahoo.com>
2017-04-11 05:51:51 -0700

    added info field column to Products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411123822) do
+ActiveRecord::Schema.define(version: 20170411124833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170411123822) do
     t.decimal  "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "info"
   end
 
   create_table "purchases", force: :cascade do |t|
