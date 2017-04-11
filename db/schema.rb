f94b4aea96fbdded7291fa48b8b30f0ef888e97a
timfesta <timothyfesta@yahoo.com>
2017-04-11 05:41:36 -0700

    created product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410131401) do
+ActiveRecord::Schema.define(version: 20170411123822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "image_name"
+    t.string   "sku"
+    t.string   "download_url"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
