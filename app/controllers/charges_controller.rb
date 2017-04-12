fcf2b4899c0ecb805780005d33de23e4c1166e65
timfesta <timothyfesta@yahoo.com>
2017-04-12 06:24:38 -0700

    charges controller and seeded the db

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,29 @@
+class ChargesController < ApplicationController
+
+	def create
+		product = Product.find(params[:product_id]) 
+	  # Amount in cents
+	 
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :source  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => product.price_in_cents,
+	    :description => 'Rails Stripe customer',
+	    :currency    => 'usd'
+	  )
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+	  	amount: product.price_in_cents, description: charge.description, currency: charge.currency,
+	  	customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+
+	  redirect_to purchase
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to new_charge_path
+	end
+	
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+firstPhoto = Product.create(title: "Russian Hill", 
+ subtitle: "Russian Hill", image_name: "bay.jpg",
+ price: "5.99", sku: "sfOne", info: "Scenic", download_url: "https://s3-us-west-2.amazonaws.com/vividkick/VK+Wedding+Foilage.lrtemplate", 
+  description: %{<p>sf</p>
+ })
